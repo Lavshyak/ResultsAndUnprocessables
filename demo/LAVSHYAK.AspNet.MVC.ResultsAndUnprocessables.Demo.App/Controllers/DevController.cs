64270bc1ad21bf73006a3e5b633ecb9a6ac74bb4
Lavshyak <lavshyak@gmail.com>
2025-01-05 01:51:19 +0500

    refactor solution, add demo

---
+++ demo/LAVSHYAK.AspNet.MVC.ResultsAndUnprocessables.Demo.App/Controllers/DevController.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel;
+using System.Net.Mime;
+using LAVSHYAK.AspNet.MVC.ResultsAndUnprocessables.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LAVSHYAK.AspNet.MVC.ResultsAndUnprocessables.Demo.App.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class DevController : ControllerBase
+{
+    public enum SomeErrors
+    {
+        [Description("A error")] A,
+        B
+    }
+
+    [HttpGet]
+    public ResultOrUnprocessable<FileResult, SomeErrors> GetFileOrError()
+    {
+        return File([2, 3, 5, 6, 2], MediaTypeNames.Application.Octet);
+    }
+
+    [HttpGet]
+    public ResultOrUnprocessable<string, SomeErrors> GetError()
+    {
+        return SomeErrors.A;
+    }
+
+    [HttpGet]
+    public ResultOrUnprocessable<string, SomeErrors> GetString()
+    {
+        return "xdd";
+    }
+
+    [HttpGet]
+    public async Task<ResultOrUnprocessable<string, SomeErrors>> GetErrorA()
+    {
+        await Task.Delay(10);
+        return SomeErrors.A;
+    }
+
+    [HttpGet]
+    public async Task<ResultOrUnprocessable<string, SomeErrors>> GetStringA()
+    {
+        await Task.Delay(10);
+        return "xdd";
+    }
+
+    [HttpGet]
+    public async Task<SuccessOrUnprocessable<SomeErrors>> GetSError()
+    {
+        await Task.Delay(10);
+        return SomeErrors.A;
+    }
+
+    [HttpGet]
+    public async Task<SuccessOrUnprocessable<SomeErrors>> GetSSuccess()
+    {
+        await Task.Delay(10);
+        return SUCCESS;
+    }
+}
---
+++ demo/LAVSHYAK.AspNet.MVC.ResultsAndUnprocessables.Demo.App/Program.cs
@@ -0,0 +1,39 @@
+using LAVSHYAK.AspNet.MVC.ResultsAndUnprocessables.OperationFilters;
+using LAVSHYAK.AspNet.MVC.ResultsAndUnprocessables.OutputFormatters;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+
+services.AddControllers(options =>
+{
+    options.OutputFormatters.RemoveType<StringOutputFormatter>(); // suddenly it won't work without it
+    options.OutputFormatters.Insert(0, new SuccessOrUnprocessableOutputFormatter());
+    options.OutputFormatters.Insert(0, new ResultOrUnprocessableOutputFormatter());
+});
+
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Your API V1", Version = "v1" });
+
+    var basePath = Directory.GetCurrentDirectory();
+
+    options.IncludeXmlComments(Path.Combine(basePath, "LAVSHYAK.AspNet.MVC.ResultsAndUnprocessables.Demo.App.xml"),
+        true); //for documentation of models, etc.
+
+    options.OperationFilter<ResultOrUnprocessableOperationFilter>();
+    options.OperationFilter<SuccessOrUnprocessableOperationFilter>();
+    options.OperationFilter<FileResultOperationFilter>();
+});
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapControllers();
+
+app.Run();
